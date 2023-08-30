bff1d658b45bad29e986e872b6e9b2909947a332
younesse el-mouden <dayouness187@gmail.com>
2023-08-30 19:31:35 +0000

    Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -38,20 +38,44 @@ int binary_tree_balance(const binary_tree_t *tree)
 	return (l - r);
 }
 /**
+ * binary_tree_size - function that measures the size of a binary tree
+ * @tree: is a pointer to the root node of the tree to measure the size
+ * Return: the size of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t l = 0, r = 0;
+
+	if (!tree)
+		return (0);
+
+	l = binary_tree_size(tree->left);
+	r = binary_tree_size(tree->right);
+
+	return (l + r + 1);
+}
+/**
  * perf - checks if the tree is perfect
  * @tree: a pointer to a tree
  * Return: 1 if is perfect or 0 if not
  */
 int perf(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (1);
+	int h, s, p, i;
 
-	if ((tree->left && tree->right) || (tree->left == tree->right))
-	{
-		return (binary_tree_is_perfect(tree->left) &&
-		binary_tree_is_perfect(tree->right));
-	}
+	if (!tree)
+		return (0);
+
+	h = (int)binary_tree_height(tree) + 1;
+	s = (int)binary_tree_size(tree);
+
+	p = 1;
+
+	for (i = 0; i < h; i++)
+		p *= 2;
+
+	if (s == p - 1)
+		return (1);
 
 	return (0);
 }
