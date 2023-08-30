dad5ee5d8e75f63943f1869990dfd52b60a2a4e7
younesse el-mouden <dayouness187@gmail.com>
2023-08-30 19:06:09 +0000

    Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -50,13 +50,30 @@ int binary_tree_balance(const binary_tree_t *tree)
 	return (left_height - right_height);
 }
 /**
+ * perf - checks if the tree is perfect
+ * @tree: a pointer to a tree
+ * Return: 1 if is perfect or 0 if not*/
+int perf(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (1);
+
+	if ((tree->left && tree->right) || (tree->left == tree->right))
+	{
+		return (binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right));
+	}
+
+	return (0);
+}
+/**
  * binary_tree_is_perfect - function that checks if a binary tree is perfect
  * @tree: is a pointer to the root node of the tree to check
  * Return: 1 if perfect or 0 if not
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (binary_tree_balance(tree) == 0)
+	if (binary_tree_balance(tree) == 0 && perf(tree))
 		return (1);
 	return (0);
 }
