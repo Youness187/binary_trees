ceaf99fb8e8c8fde0ccf25fe15c147fd2d6dd7ef
younesse el-mouden <dayouness187@gmail.com>
2023-08-31 15:39:01 +0000

    Level-order traversal

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_levelorder - goes through a B-T using level-order traversal
+ * @tree: is a pointer to the root node of the tree to traverse
+ * @func: is a pointer to a function to call for each node
+*/
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	if (!tree->parent)
+		func(tree->n);
+	if (tree->left)
+		func(tree->left->n);
+	if (tree->right)
+		func(tree->right->n);
+
+	binary_tree_levelorder(tree->left, func);
+	binary_tree_levelorder(tree->right, func);
+}
