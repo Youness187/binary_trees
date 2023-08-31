77129c1900666f7d6ac20b6c870d9ade5edc2757
younesse el-mouden <dayouness187@gmail.com>
2023-08-31 15:22:20 +0000

    Lowest common ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -13,15 +13,14 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 
 	if (first == second)
 		return ((binary_tree_t *)first);
-	if (second->parent == first)
-		return ((binary_tree_t *)first);
-	if (first->parent == second)
-		return ((binary_tree_t *)second);
-	if (second->parent == first->parent)
-		return (first->parent);
-	if (!first->parent && second->parent)
+
+	if (!first->parent || (!first->parent->parent && second->parent)
+		|| second->parent == first)
 		return (binary_trees_ancestor(first, second->parent));
-	if (!second->parent && first->parent)
+
+	else if (!second->parent || (!second->parent->parent && first->parent)
+		|| first->parent == second )
 		return (binary_trees_ancestor(first->parent, second));
+
 	return (binary_trees_ancestor(first->parent, second->parent));
 }
